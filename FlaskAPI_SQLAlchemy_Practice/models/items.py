bd3f49343df4f475535827b8086a7c13a6a2b2ff
PraveenGithubCse <98269331+PraveenGithubCse@users.noreply.github.com>
2025-11-30 19:10:59 +0530

    Updated items table with new column "quantity" items.py

--- FlaskAPI_SQLAlchemy_Practice/models/items.py
+++ FlaskAPI_SQLAlchemy_Practice/models/items.py
@@ -7,6 +7,7 @@ class ItemModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float(precision=2), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
 
     store_id = db.Column(
         db.Integer, db.ForeignKey("stores.id"), unique=False, nullable=False
